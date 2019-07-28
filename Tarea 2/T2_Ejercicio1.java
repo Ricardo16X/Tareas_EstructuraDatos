85ee06c125ad0db09f6fb3a578e16c6e12dfb38f
Ricardo <perezricardo11@yahoo.com>
2019-07-27 21:14:31 -0600

    Tarea 2, Lista circular

--- Tarea 2/T2_Ejercicio1.java
+++ Tarea 2/T2_Ejercicio1.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import jdk.internal.dynalink.beans.StaticClass;
-
 public class T2_Ejercicio1 {
 
 	static int numeroBolsa = 0;
@@ -20,29 +18,37 @@ public class T2_Ejercicio1 {
 		// Creando la lista inicial de bolsas
 		listaBolsa listitaBolsa = new listaBolsa();
 
-		/*
-		 * ------------PRUEBAS----------------- listitaBolsa.bolsaVacia(new
-		 * nodoBolsa()); listitaBolsa.bolsaVacia(new nodoBolsa());
-		 * listitaBolsa.bolsaVacia(new nodoBolsa()); listitaBolsa.bolsaVacia(new
-		 * nodoBolsa()); listitaBolsa.bolsaVacia(new nodoBolsa());
-		 * 
-		 * nodoBolsa bolsa1 = listitaBolsa.obtenerBolsa(1);
-		 * System.out.println(listitaBolsa.esVacia(bolsa1));
-		 * listitaBolsa.insertar(bolsa1, "Elemento 1");
-		 * System.out.println(listitaBolsa.esVacia(bolsa1));
-		 * listitaBolsa.insertar(bolsa1, "Elemento 2"); listitaBolsa.insertar(bolsa1,
-		 * "Elemento 3");
-		 */
-
+		// ------------PRUEBAS-----------------
+//		  listitaBolsa.bolsaVacia(new nodoBolsa()); 
+//		  listitaBolsa.bolsaVacia(new nodoBolsa());
+//		  listitaBolsa.bolsaVacia(new nodoBolsa()); 
+//		  listitaBolsa.bolsaVacia(new nodoBolsa()); 
+//		  listitaBolsa.bolsaVacia(new nodoBolsa());
+//		  
+//		  nodoBolsa bolsa1 = listitaBolsa.obtenerBolsa(1);
+//		  System.out.println(listitaBolsa.esVacia(bolsa1));
+//		  listitaBolsa.insertar(bolsa1, "Elemento 1");
+//		  System.out.println(listitaBolsa.esVacia(bolsa1));
+//		  listitaBolsa.insertar(bolsa1, "Elemento 2"); listitaBolsa.insertar(bolsa1,
+//		  "Elemento 3");
+//		 
+//		  System.out.println(listitaBolsa.mostrarBolsas());
 		// crear un menu inicial
 		while (opcion != 3) {
 			System.out.println(
 					"Menu Inicial\n" + "1. Crear una nueva Bolsa\n" + "2. Elegir Bolsa\n" + "3. Salir Aplicacion");
-			opcion = lectura.nextInt();
+			try {
+				opcion = Integer.parseInt(lectura.nextLine()); 
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("Ingresa el número para seleccionar una opción.");
+				opcion = 0;
+			}
 			// Verificando la opción elegida
 			switch (opcion) {
 			case 1:
 				listitaBolsa.bolsaVacia(new nodoBolsa());
+				System.out.println("Bolsa creada!\n");
 				break;
 			case 2:
 				cantidad = listitaBolsa.cantidadBolsas();
@@ -50,45 +56,84 @@ public class T2_Ejercicio1 {
 					opcion = 0;
 					while (opcion <= 0 | opcion > cantidad) {
 						System.out.println(listitaBolsa.mostrarBolsas());
-						opcion = lectura.nextInt();
+						try {
+							opcion = Integer.parseInt(lectura.nextLine());
+							
+						} catch (Exception e) {
+							// TODO: handle exception
+							System.out.println("Ingresa solamente numeros enteros. " + e.getMessage());
+							opcion = 0;
+						}
+						
 					}
 					// Aqui paso como parámetro el numero de bolsa elegida
 					temporal = listitaBolsa.obtenerBolsa(opcion);
 					// Una vez obtenida la bolsa a trabajar,proceso a preguntar que desea hacer
 					// Menu de acciones para la bolsa elegida
 					opcion = 0;
-					while (opcion != 4) {
+					while (opcion != 5) {
 						System.out.println("Menu Bolsa\n" + "1. Insertar\n" + "2. Contar Elementos\n"
-								+ "3. Verificar Vacio\n" + "4. Regresar");
+								+ "3. Verificar si esta Vacio\n" + "4. Vaciar Bolsa\n" + "5. Regresar\n");
 						try {
-							opcion = lectura.nextInt();
+							opcion = Integer.parseInt(lectura.nextLine());
+							
 							switch (opcion) {
-							case 1:
-								// Basicamente, solo se pueden insertar Strings
+							case 1: // Basicamente todo lo pone en string, solo se pueden insertar "nombres de
+									// elementos"
 								System.out.println("Ingrese el nombre del elemento");
 								try {
 									entradaTeclado = lectura.next();
+									lectura.nextLine();
 									listitaBolsa.insertar(temporal, entradaTeclado);
 								} catch (Exception e) {
 									// TODO: handle exception
 									System.out.println("Error :| " + e.getMessage());
 								}
 								break;
-							case 2:
-								System.out.println("Ingrese nombre de elemento a contar");
-								try {
-									entradaTeclado = lectura.next();
-									listitaBolsa.cuantos(temporal, entradaTeclado);
-								} catch (Exception e) {
-									// TODO: handle exception
-									System.out.println("Error :| " + e.getMessage());
+							case 2: // Contar Elementos
+								if (!listitaBolsa.esVacia(temporal)) {
+									System.out.println("Ingrese desde que numero de elemento empezar a contar");
+									try {
+										opcion = Integer.parseInt(lectura.nextLine());
+										while (opcion < 0) {
+											System.out.println(
+													"Empiezo a contar desde 0\n" + "Ingresa un numero entero mayor a 0");
+											opcion = lectura.nextInt();
+											lectura.nextLine();
+										}
+										int cantidadElementos = listitaBolsa.cuantos(temporal, opcion);
+										if (cantidadElementos == 0) {
+											System.out.println("Este es el último elemento de la bolsa.\n"
+													+ "Hay 0 elementos después de este.\n");
+										} else if (cantidadElementos < 0) {
+											System.out.println(
+													"El numero ingresado es mayor, al numero de elementos en esta bolsa.\n"
+															+ "Intenta con otro valor la proxima vez.\n");
+										} else {
+											System.out.println(
+													"Hay " + cantidadElementos + " después del 'indice' ingresado.\n");
+										}
+									} catch (Exception e) {
+										// TODO: handle exception
+										System.out.println("Error :| " + e.getMessage());
+									}
+								}else {
+									System.out.println("La bolsa está vacia.");
 								}
 								break;
-							case 3:
+							case 3: // Verificar si está vacio
 								if (listitaBolsa.esVacia(temporal)) {
 									System.out.println("La bolsa " + temporal.numero + " está vacío.\n");
 								} else {
 									System.out.println("La bolsa " + temporal.numero + " no está vacío.\n");
+								}
+								break;
+							case 4:
+								if (!listitaBolsa.esVacia(temporal)) {
+									System.out.println("La bolsa " + temporal.numero + " se ha vaciado.");
+									listitaBolsa.vaciarBolsa(temporal);
+								}else {
+									System.out.println("No se puede vaciar, algo que está vacio.\n");
 								}
 								break;
 							default:
@@ -99,12 +144,15 @@ public class T2_Ejercicio1 {
 							System.out.println("Error :| " + e.getMessage());
 						}
 					}
+				} else {
+					System.out.println("No hay bolsas creadas!\n");
 				}
 				break;
 			default:
 				break;
 			}
 		}
+		lectura.close();
 	}
 
 	static class listaBolsa {
@@ -136,21 +184,41 @@ public class T2_Ejercicio1 {
 		}
 
 		// Método para crear una nueva bolsa y agregarla en la lista de bolsas.
+		// Se comportará como una lista circular
 		public void bolsaVacia(nodoBolsa bolsita) {
 			trabajoBolsa = anclaBolsa;
-			while (trabajoBolsa.siguienteBolsa != null) {
-				trabajoBolsa = trabajoBolsa.siguienteBolsa;
-			}
-			try {
-				bolsita.siguienteBolsa = null;
-				trabajoBolsa.siguienteBolsa = bolsita;
-			} catch (Exception e) {
-				// TODO: handle exception
-				System.out.println("Error al insertar Bolsa " + e.getMessage());
+			// Verifico que si el ancla apunta a nulo
+			// Si apunta a nulo, entonces no hay ninguna bolsa...
+			// La ultima bolsa será la primera
+			if (trabajoBolsa.siguienteBolsa == null) {
+				try {
+					trabajoBolsa.siguienteBolsa = bolsita;
+					trabajoBolsa.ultimaBolsa = bolsita;
+					trabajoBolsa.ultimaBolsa.siguienteBolsa = anclaBolsa.siguienteBolsa;
+					trabajoBolsa.ultimaBolsa = null;
+				} catch (Exception e) {
+					// TODO: handle exception
+					System.out.println("Error al insertar Bolsa " + e.getMessage());
+				}
+			} else {
+				do {
+					trabajoBolsa = trabajoBolsa.siguienteBolsa;
+					trabajoBolsa.ultimaBolsa = null;
+				} while (trabajoBolsa.siguienteBolsa != anclaBolsa.siguienteBolsa);
+				try {
+					trabajoBolsa.siguienteBolsa = bolsita;
+					trabajoBolsa.ultimaBolsa = bolsita;
+					trabajoBolsa.ultimaBolsa.siguienteBolsa = anclaBolsa.siguienteBolsa;
+					trabajoBolsa.ultimaBolsa = null;
+				} catch (Exception e) {
+					// TODO: handle exception
+					System.out.println("Error al insertar Bolsa " + e.getMessage());
+				}
 			}
 		}
 
 		// Método para insertar un elemento en cierta bolsa
+		// Los elementos en la lista de elementos, se comportan como una lista simple.
 		public nodoBolsa insertar(nodoBolsa bolsita, Object elemento) {
 			if (elemento != null & bolsita != null) {
 				try {
@@ -172,22 +240,15 @@ public class T2_Ejercicio1 {
 			}
 		}
 
-		// Método que cuenta cuántos elementos de "cierto tipo" hay en la bolsa
-		public int cuantos(nodoBolsa bolsita, Object elemento) {
-			// Contador
-			int contador = 0;
-			// El objeto elemento lo compararé con elementos string
-			// ejemplo elemento.toString() con cada elemento de bolsita
-			trabajoElemento = bolsita.listaElemento.anclaElemento;
-			while (trabajoElemento.siguientElemento != null) {
-				trabajoElemento = trabajoElemento.siguientElemento;
-				// Si el "elemento" de la bolsa, coincide con el "elemento" por parámetro
-				// Aumentaré en 1 el contador de elementos coincidentes.
-				if (trabajoElemento.elemento.toString().equalsIgnoreCase(elemento.toString())) {
-					contador++;
-				}
-			}
-			return contador;
+		// Método que cuenta cuántos elementos hay después de n elemento
+		public int cuantos(nodoBolsa bolsita, int indice) {
+			return cuantosElementos(bolsita) - indice;
+		}
+
+		// Método que me servirá para vaciar una bolsa de sus elementos
+		public void vaciarBolsa(nodoBolsa temporal) {
+			// TODO Auto-generated method stub
+			temporal.listaElemento.anclaElemento.siguientElemento = null;
 		}
 
 		/*-------------------------METODOS ADICIONALES-------------------------*/
@@ -196,13 +257,13 @@ public class T2_Ejercicio1 {
 			// TODO Auto-generated method stub
 			trabajoBolsa = anclaBolsa;
 			nodoBolsa tempBolsa = null;
-			while (trabajoBolsa.siguienteBolsa != null) {
+			do {
 				trabajoBolsa = trabajoBolsa.siguienteBolsa;
 				if (trabajoBolsa.numero == i) {
 					tempBolsa = trabajoBolsa;
 					break;
 				}
-			}
+			} while (trabajoBolsa.siguienteBolsa != anclaBolsa.siguienteBolsa);
 			return tempBolsa;
 		}
 
@@ -211,9 +272,11 @@ public class T2_Ejercicio1 {
 			// TODO Auto-generated method stub
 			int bolsas = 0;
 			trabajoBolsa = anclaBolsa;
-			while (trabajoBolsa.siguienteBolsa != null) {
-				trabajoBolsa = trabajoBolsa.siguienteBolsa;
-				bolsas++;
+			if (trabajoBolsa.siguienteBolsa != null) {
+				do {
+					trabajoBolsa = trabajoBolsa.siguienteBolsa;
+					bolsas++;
+				} while (trabajoBolsa.siguienteBolsa != anclaBolsa.siguienteBolsa);
 			}
 			return bolsas;
 		}
@@ -223,12 +286,26 @@ public class T2_Ejercicio1 {
 			String cadena = "Elige una bolsa\n";
 			int contador = 0;
 			trabajoBolsa = anclaBolsa;
-			while (trabajoBolsa.siguienteBolsa != null) {
+			do {
 				trabajoBolsa = trabajoBolsa.siguienteBolsa;
 				contador++;
-				cadena += contador + ". Bolsa " + contador + "\n";
-			}
+				cadena += contador + ". Bolsa " + trabajoBolsa.numero + "\n";
+			} while (trabajoBolsa.siguienteBolsa != anclaBolsa.siguienteBolsa);
 			return cadena;
+		}
+
+		// Metodo que me regresa cuantos elementos tiene una bolsa
+		private int cuantosElementos(nodoBolsa bolsita) {
+			// TODO Auto-generated method stub
+			// Variable contadora de elementos
+			int contador = 0;
+			// Recorre todos los elementos y los va contando.
+			trabajoElemento = bolsita.listaElemento.anclaElemento;
+			while (trabajoElemento.siguientElemento != null) {
+				trabajoElemento = trabajoElemento.siguientElemento;
+				contador++;
+			}
+			return contador;
 		}
 	}
 
@@ -237,6 +314,7 @@ public class T2_Ejercicio1 {
 		// cada bolsa tendra una lista de elementos
 		int numero;
 		nodoBolsa siguienteBolsa;
+		nodoBolsa ultimaBolsa;
 		listaElemento listaElemento;
 
 		public nodoBolsa() {
